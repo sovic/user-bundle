167d99f321a89ead02a05f3266673a29a5230485
Vojtěch Bojda <info@vojtechbojda.cz>
2024-08-07 22:40:42 +0200

    updates user form

--- src/Form/Type/User.php
+++ src/Form/Type/User.php
@@ -23,54 +23,57 @@ class User extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder->setMethod('POST');
-        $builder
-            ->add(
-                'email',
-                EmailType::class,
-                [
-                    'label' => 'user.edit.form.email',
-                    'attr' => [
-                        'placeholder' => 'user.edit.form.email',
-                    ],
-                    'required' => true,
-                ]
-            )
-            ->add(
-                'is_enabled',
-                CheckboxType::class,
-                [
-                    'label' => 'user.edit.form.is_enabled',
-                    'required' => false,
-                ]
-            )
-            ->add(
-                'username',
-                TextType::class,
-                [
-                    'label' => 'user.edit.form.username',
-                    'attr' => [
-                        'placeholder' => 'user.edit.form.username',
-                    ],
-                    'required' => false,
-                ]
-            )
-            ->add(
-                'is_emailing_enabled',
-                CheckboxType::class,
-                [
-                    'label' => 'user.edit.form.is_emailing_enabled',
-                    'required' => false,
-                ]
-            )
-            ->add(
-                'save',
-                SubmitType::class,
-                [
-                    'label' => 'user.edit.form.submit',
-                    'attr' => [
-                        'class' => FormSettings::SUBMIT_CLASS,
-                    ],
-                ]
-            );
+        $builder->add(
+            'email',
+            EmailType::class,
+            [
+                'label' => 'user.edit.form.email',
+                'attr' => [
+                    'placeholder' => 'user.edit.form.email',
+                ],
+                'required' => true,
+            ]
+        );
+
+        $builder->add(
+            'is_enabled',
+            CheckboxType::class,
+            [
+                'label' => 'user.edit.form.is_enabled',
+                'required' => false,
+            ]
+        );
+
+        $builder->add(
+            'username',
+            TextType::class,
+            [
+                'label' => 'user.edit.form.username',
+                'attr' => [
+                    'placeholder' => 'user.edit.form.username',
+                ],
+                'required' => false,
+            ]
+        );
+
+        $builder->add(
+            'is_emailing_enabled',
+            CheckboxType::class,
+            [
+                'label' => 'user.edit.form.is_emailing_enabled',
+                'required' => false,
+            ]
+        );
+
+        $builder->add(
+            'save',
+            SubmitType::class,
+            [
+                'label' => 'user.edit.form.submit',
+                'attr' => [
+                    'class' => FormSettings::SUBMIT_CLASS,
+                ],
+            ]
+        );
     }
 }
