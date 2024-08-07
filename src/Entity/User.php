230eb33425530a5963fa7790d13206a28b6d6707
Vojtěch Bojda <info@vojtechbojda.cz>
2024-08-07 22:44:40 +0200

    updates user entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,7 +7,6 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\Id;
-use Doctrine\ORM\Mapping\MappedSuperclass;
 use Sovic\Common\Entity\Trait\CreatedAtTrait;
 use Sovic\Common\Entity\Trait\DeletedAtTrait;
 use Sovic\Common\Enum\CountryId;
@@ -21,7 +20,6 @@ if (!defined('GLOBAL_ENTITY_ID_STRATEGY')) {
     define('GLOBAL_ENTITY_ID_STRATEGY', 'AUTO');
 }
 
-#[MappedSuperclass]
 class User implements UserInterface, UserEntityInterface, PasswordAuthenticatedUserInterface
 {
     use CreatedAtTrait;
